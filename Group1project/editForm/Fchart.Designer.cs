8a698f14aa49328fd3be54a78dd7dcdc6dcdfd46
LinearKangaroo <aung378629@gmail.com>
2026-03-07 05:19:03 +0630

    added analytic chart form

--- Group1project/editForm/Fchart.Designer.cs
+++ Group1project/editForm/Fchart.Designer.cs
@@ -28,12 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Fchart";
+            uiLineChart1 = new Sunny.UI.UILineChart();
+            pnlBtm.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlBtm
+            // 
+            pnlBtm.Location = new Point(1, 509);
+            pnlBtm.Size = new Size(1003, 55);
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(875, 12);
+            // 
+            // btnOK
+            // 
+            btnOK.Location = new Point(760, 12);
+            // 
+            // uiLineChart1
+            // 
+            uiLineChart1.Font = new Font("Microsoft Sans Serif", 12F);
+            uiLineChart1.LegendFont = new Font("Microsoft Sans Serif", 9F);
+            uiLineChart1.Location = new Point(26, 58);
+            uiLineChart1.MinimumSize = new Size(1, 1);
+            uiLineChart1.MouseDownType = Sunny.UI.UILineChartMouseDownType.Zoom;
+            uiLineChart1.Name = "uiLineChart1";
+            uiLineChart1.Size = new Size(975, 443);
+            uiLineChart1.SubFont = new Font("Microsoft Sans Serif", 9F);
+            uiLineChart1.TabIndex = 2;
+            uiLineChart1.Text = "uiLineChart1";
+            // 
+            // Fchart
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(1005, 567);
+            Controls.Add(uiLineChart1);
+            Name = "Fchart";
+            Text = "Analytic Chart";
+            Controls.SetChildIndex(pnlBtm, 0);
+            Controls.SetChildIndex(uiLineChart1, 0);
+            pnlBtm.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Sunny.UI.UILineChart uiLineChart1;
     }
 }
